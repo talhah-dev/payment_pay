a59004c97590769b40928ee80ff5ec5ae4b40cc3
Muhammad Talha <talha18513@gmail.com>
2025-07-09 04:57:13 -0700

    ui section created

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -49,7 +49,6 @@ import {
     useReactTable,
     VisibilityState,
 } from "@tanstack/react-table"
-import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
 import { z } from "zod"
 
 import { useIsMobile } from "@/hooks/use-mobile"
@@ -541,15 +540,6 @@ export function DataTable({
         </Tabs>
     )
 }
-
-const chartData = [
-    { month: "January", desktop: 186, mobile: 80 },
-    { month: "February", desktop: 305, mobile: 200 },
-    { month: "March", desktop: 237, mobile: 120 },
-    { month: "April", desktop: 73, mobile: 190 },
-    { month: "May", desktop: 209, mobile: 130 },
-    { month: "June", desktop: 214, mobile: 140 },
-]
 
 // const chartConfig = {
 //     desktop: {
